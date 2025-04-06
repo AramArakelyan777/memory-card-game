0c944ed32aaebaf040c20f9e19452107cc534eb1
Aram Arakelyan <aram777arakelyan@gmail.com>
2025-04-06 13:01:44 +0400

    created game global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import React from "react"
+import React, { useContext } from "react"
+import { LevelButton } from "./components/LevelButton"
+import { GameContext } from "./context/GameContext"
 
 function App() {
-    return <React.Fragment>Memory Card Game</React.Fragment>
+    const { gameState } = useContext(GameContext)
+
+    const handleLevelButtonClick = (level) => {}
+
+    return (
+        <React.Fragment>
+            <h1>Memory Card Game</h1>
+            <LevelButton
+                level="easy"
+                onClick={(e) => {
+                    e.preventDefault()
+                    handleLevelButtonClick("easy")
+                }}
+            />
+            <LevelButton
+                level="medium"
+                onClick={(e) => {
+                    e.preventDefault()
+                    handleLevelButtonClick("medium")
+                }}
+            />
+            <LevelButton
+                level="hard"
+                onClick={(e) => {
+                    e.preventDefault()
+                    handleLevelButtonClick("hard")
+                }}
+            />
+        </React.Fragment>
+    )
 }
 
 export default App
